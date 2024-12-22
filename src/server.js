7aa4bee997bfdb54463b92cb41b035937ad043ff
Abdus Sadik Molla <sadik.only4work@gmail.com>
2024-12-22 13:53:51 +0530

    add API routing with versioning and configure middleware for JSON and URL-encoded data

--- src/server.js
+++ src/server.js
@@ -2,10 +2,14 @@ import express from 'express';
 
 import connectDB from './configs/db.config.js';
 import configs from './configs/server.config.js';
+import apiRouter from './routes/api.routes.js';
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
+app.use('/api',apiRouter);
 
 app.listen(configs.PORT,()=>{
     console.log(`Server is running on port ${configs.PORT}`);
