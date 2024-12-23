e7b22cc7503d85c9c5eac49fc28c026f91468e70
Abdus Sadik Molla <sadik.only4work@gmail.com>
2024-12-23 08:40:43 +0530

    added jwt access and refresh secret token to server.config.js

--- src/configs/server.config.js
+++ src/configs/server.config.js
@@ -6,7 +6,9 @@ const configs = {
   NODE_ENV: process.env.NODE_ENV,
   get DB_URL(){
     return this.NODE_ENV==='development'? process.env.DEV_DB_URL : process.env.PROD_DB_URL;
-  }
+  },
+  JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
+  JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET
 }
 
 export default configs;
