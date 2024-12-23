a056140c2e18cbd274045f4ff2caa8ceefbfb06e
Abdus Sadik Molla <sadik.only4work@gmail.com>
2024-12-23 07:53:30 +0530

    added create, getAll, getById, update, delete repository function to crud.repository.js

--- src/repository/crud.repository.js
+++ src/repository/crud.repository.js
@@ -7,7 +7,61 @@ class CrudRepository {
     return this.model.create(data);
   }
 
-  async findAll(populateOptions = null, selectOptions = null) {
-    // TODO: implement find all with populate and select options
+  async getAll(populateOptions = null, selectOptions = null) {
+    const query = this.model.find();
+    if(populateOptions){
+      if(Array.isArray(populateOptions)){
+        populateOptions.forEach(option => {
+          query.populate(option);
+        });
+      }else{
+        query.populate(populateOptions);
+      }
+    }
+    if(selectOptions){
+      query.select(selectOptions);
+    }
+    return await query;
+  }
+
+  async getById(id,populateOptions = null, selectOptions = null) {
+    const query = this.model.findById(id);
+    if(populateOptions){
+      if(Array.isArray(populateOptions)){
+        populateOptions.forEach(option => {
+          query.populate(option);
+        });
+      }else{
+        query.populate(populateOptions);
+      }
+    }
+    if(selectOptions){
+      query.select(selectOptions);
+    }
+    return await query;
+  }
+
+  async update(id,data,populateOptions = null, selectOptions = null) {
+    const query = this.model.findByIdAndUpdate(id,data,{new:true});
+    if(populateOptions){
+      if(Array.isArray(populateOptions)){
+        populateOptions.forEach(option => {
+          query.populate(option);
+        });
+      }else{
+        query.populate(populateOptions);
+      }
+    }
+    if(selectOptions){
+      query.select(selectOptions);
+    }
+    return await query;
+  }
+
+  async delete(id) {
+    return await this.model.findByIdAndDelete(id);
   }
 }
+
+
+export default CrudRepository;
