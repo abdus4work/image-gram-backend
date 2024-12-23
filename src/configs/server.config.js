46fa10ac785d6d0cfbccefed625714db6eb246c4
Abdus Sadik Molla <sadik.only4work@gmail.com>
2024-12-23 08:48:57 +0530

    added jwt expirtion variable to server.config.js

--- src/configs/server.config.js
+++ src/configs/server.config.js
@@ -8,7 +8,10 @@ const configs = {
     return this.NODE_ENV==='development'? process.env.DEV_DB_URL : process.env.PROD_DB_URL;
   },
   JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
-  JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET
+  JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
+  JWT_ACCESS_EXPIRY: process.env.JWT_ACCESS_EXPIRATION,
+  JWT_REFRESH_EXPIRY: process.env.JWT_REFRESH_EXPIRATION
+
 }
 
 export default configs;
