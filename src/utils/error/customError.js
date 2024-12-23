64301e923ad74b52f1c6038046d199777d2da792
Abdus Sadik Molla <sadik.only4work@gmail.com>
2024-12-23 11:07:14 +0530

    added custom error class

---
+++ src/utils/error/customError.js
@@ -0,0 +1,33 @@
+import configs from '../../configs/serverConfig.js'
+
+class CustomError extends Error{
+  constructor(
+    statusCode,
+    errorCode='ERR_GENERIC',
+    message='Something went wrong',
+    details=null
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.errorCode = errorCode;
+    this.message = message;
+    this.details = details;
+    this.data = {};
+
+    Error.captureStackTrace(this,this.constructor);
+  }
+
+  toJSON(){
+    return {
+      success: false,
+      statusCode: this.statusCode,
+      errorCode: this.errorCode,
+      message: this.message,
+      details: this.details,
+      data: this.data,
+      stackTrace: configs.NODE_ENV === 'development' ? this.stack : null
+    }
+  }
+}
+
+export default CustomError;
