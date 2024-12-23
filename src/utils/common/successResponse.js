7599348ff3e62679b0e646361ce3993940e9a2d1
Abdus Sadik Molla <sadik.only4work@gmail.com>
2024-12-23 18:01:43 +0530

    added success response class to successResponse.js

---
+++ src/utils/common/successResponse.js
@@ -0,0 +1,24 @@
+class SuccessResponse{
+  constructor(
+    statusCode=200,
+    message='Success',
+    data=null,
+  ) {
+    this.statusCode = statusCode;
+    this.message = message;
+    this.data = data;
+    this.timestamp = new Date().toISOString();
+  }
+
+  sendResponse(){
+    return{
+      success: true,
+      statusCode: this.statusCode,
+      message: this.message,
+      data: this.data,
+      timestamp: this.timestamp,
+    }
+  }
+}
+
+export default SuccessResponse;
