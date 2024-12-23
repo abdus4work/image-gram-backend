247ae598d2e1e4b709eebc2a7fce6e944123ce46
Abdus Sadik Molla <sadik.only4work@gmail.com>
2024-12-23 18:47:21 +0530

    added get profile function for user

--- src/routes/v1/userRoutes.js
+++ src/routes/v1/userRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
+
 import UserController from '../../controller/userController.js';
 
-const userRouter= express.Router();
+const userRouter = express.Router();
 const userController = new UserController();
-userRouter.get('/:username',userController.getProfile)
+userRouter.get('/:username', userController.getProfile);
 
 export default userRouter;
