e9cfbb937e863e4f11e1afbae08b548f195f05f2
Abdus Sadik Molla <sadik.only4work@gmail.com>
2024-12-23 07:59:23 +0530

    feat: add crud and user repository function
    Includes create, getAll, getById, update, delete and getByEmailOrUsername repository functions

---
+++ src/repository/user.repository.js
@@ -0,0 +1,12 @@
+import userModel from '../models/user.model.js';
+import CrudRepository from './crud.repository.js';
+
+class UserRepository extends CrudRepository {
+  constructor() {
+    super(userModel);
+  }
+
+  async getByEmailOrUsername(identifier){
+    return this.model.findOne({$or:[{email:identifier},{username:identifier}]}).select('-password -__V, -refreshToken');
+  }
+}
