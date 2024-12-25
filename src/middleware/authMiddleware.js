6dd3f662c72d0a0173304ef18488cd7dc6cdbc83
Abdus Sadik Molla <sadik.only4work@gmail.com>
2024-12-25 08:27:49 +0530

    added isAuthenticated middleware to authMiddleware.js

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,64 @@
+import { StatusCodes } from 'http-status-codes';
+import CustomError from '../utils/error/customError.js';
+import configs from '../configs/serverConfig.js';
+import AuthService from '../service/authService.js';
+import ErrorCodes from '../utils/error/errorCodes.js';
+
+const authService = new AuthService();
+
+class AuthMiddleware {
+  isAuthenticated(req, res, next) {
+    try {
+      const token = req.headers.authorization.split(' ')[1];
+      if (!token) {
+        return res
+          .status(StatusCodes.UNAUTHORIZED)
+          .json(
+            new CustomError(
+              StatusCodes.UNAUTHORIZED,
+              ErrorCodes.UNAUTHORIZED,
+              'Token not provided'
+            )
+          );
+      }
+
+      const decoded = authService.verifyToken(token, configs.JWT_ACCESS_SECRET);
+      req.user = decoded;
+      next();
+    } catch (err) {
+      if (err.name === 'JsonWebTokenError') {
+        return res
+          .status(StatusCodes.UNAUTHORIZED)
+          .json(
+            new CustomError(
+              StatusCodes.UNAUTHORIZED,
+              ErrorCodes.UNAUTHORIZED,
+              'Invalid token'
+            )
+          );
+      }
+      if (err.name === 'TokenExpiredError') {
+        return res
+          .status(StatusCodes.UNAUTHORIZED)
+          .json(
+            new CustomError(
+              StatusCodes.UNAUTHORIZED,
+              ErrorCodes.UNAUTHORIZED,
+              err.message
+            )
+          );
+      }
+      return res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json(
+          new CustomError(
+            StatusCodes.INTERNAL_SERVER_ERROR,
+            ErrorCodes.INTERNAL_SERVER_ERROR,
+            'Internal server error'
+          )
+        );
+    }
+  }
+}
+
+export default AuthMiddleware;
