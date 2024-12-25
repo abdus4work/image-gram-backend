84c539fa60e311e8c0f56e256e130652c1e25dc1
Abdus Sadik Molla <sadik.only4work@gmail.com>
2024-12-25 09:18:43 +0530

    added error handling middleware

--- src/server.js
+++ src/server.js
@@ -2,18 +2,19 @@ import express from 'express';
 
 import connectDB from './configs/dbConfig.js';
 import configs from './configs/serverConfig.js';
+import errorHandlingMiddleware from './middleware/errorMiddleware.js';
 import apiRouter from './routes/apiRoutes.js';
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
+app.use('/api', apiRouter);
 
-app.use('/api',apiRouter);
+app.use(errorHandlingMiddleware);
 
-
-app.listen(configs.PORT,()=>{
-    console.log(`Server is running on port ${configs.PORT}`);
-    connectDB();
-})
+app.listen(configs.PORT, () => {
+  console.log(`Server is running on port ${configs.PORT}`);
+  connectDB();
+});
