02cad8caead7b3c400dd1c9aaab756db9e9f588b
Abdus Sadik Molla <sadik.only4work@gmail.com>
2024-12-31 09:04:01 +0530

    added post model

--- src/server.js
+++ src/server.js
@@ -13,8 +13,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use('/api', apiRouter);
+app.get('/ping', (req, res) => {
+  res.status(200).json({ message: 'pong' });
+}
+);
 
 app.use(errorHandlingMiddleware);
+
 
 app.listen(configs.PORT, () => {
   console.log(`Server is running on port ${configs.PORT}`);
